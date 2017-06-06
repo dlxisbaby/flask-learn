84604efa1186a91ef0c3fe34fb811a0cf8a59288
dlxisbaby <hebe19880814@163.com>
2017-06-06 23:14:11 +0800

    111

--- main.py
+++ main.py
@@ -13,6 +13,11 @@ class User(db.Model):
     id = db.Column(db.Integer(),primary_key=True)
     username = db.Column(db.String(255))
     password = db.Column(db.String(255))
+    posts = db.relationship(
+        'Post',
+        backref = 'user',
+        lazy = 'dynamic'
+        )
     
     def __init__(self,username):
         self.username = username
@@ -26,6 +31,11 @@ class Post(db.Model):
     text = db.Column(db.Text())
     publish_data = db.Column(db.DateTime())
     user_id = db.Column(db.Integer(),db.ForeignKey('user.id'))
+    comments = db.relationship(
+        'Comment',
+        backref = 'post',
+        lazy = 'dynamic'
+        )
     
     def __init__(self,title):
         self.title = title
@@ -33,6 +43,15 @@ class Post(db.Model):
     def __repr__(self):
         return "<Post '{}'>".format(self.title)
     
+class Comment(db.model):
+    id = db.Column(db.Integer(),primary_key=True)
+    name = db.Column(db.String(255))
+    text = db.Column(db.Text())
+    date = db.Column(db.DateTime())
+    post_id = db.Column(db.Integer(),ForeignKey='post.id')
+    
+    def __repr__(self):
+        return "<Comment '{}'>".format(self.text[:15])
     
 @app.route('/')
 def home():
--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 
 from flask_script import Manager,Server
 
-from main import app,db,User
+from main import app,db,User,Post,Comment
 
 #把app传给Manager对象，以初始化Flask Script
 manager = Manager(app)
@@ -12,7 +12,7 @@ manager.add_command("server",Server())
 
 @manager.shell
 def make_shell_context():
-    return dict(app=app,db=db,User=User)
+    return dict(app=app,db=db,User=User,Post=Post,Comment=Comment)
 
 if __name__ == "__main__":
     manager.run()
